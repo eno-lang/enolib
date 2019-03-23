949dd19303b992c9225a6095e2a5672616c32bdf
Simon Repp <simon@fdpl.io>
2019-03-23 12:24:16 +0100

    Switch to exhaustive lookup testing over the full sample document range

--- javascript/specs/lookup.spec.js
+++ javascript/specs/lookup.spec.js
@@ -1,135 +1,80 @@
 const enolib = require('..');
 
-const sample = `
-color: cyan
-close:up
-# notes
--- long
-is
--- long
+
+const input = `
+field: value
+
+list:
+- item
+
+- item
+
+fieldset:
+entry = value
+
+entry = value
+
+copied_field < field
+copied_fieldset < fieldset
+copied_list < list
+
+# section
+
+-- empty
+-- empty
+
+-- multiline
+value
+value
+-- multiline
+
+copied_multiline < multiline
+
+## subsection
+## copy < subsection
 `.trim();
 
-const scenarios = [
-  {
-    element: 'color', // '|color'
-    position: [0],
-    range: 'key'
-  },
-  {
-    element: 'color', // '|color'
-    position: [0, 0],
-    range: 'key'
-  },
-  {
-    element: 'color', // 'col|or'
-    position: [3],
-    range: 'key'
-  },
-  {
-    element: 'color', // 'col|or'
-    position: [0, 3],
-    range: 'key'
-  },
-  {
-    element: 'color', // 'color:|'
-    position: [6],
-    range: 'elementOperator'
-  },
-  {
-    element: 'color', // 'color:|'
-    position: [0, 6],
-    range: 'elementOperator'
-  },
-  {
-    element: 'color', // '|close'
-    position: [7],
-    range: 'value'
-  },
-  {
-    element: 'color', // '|close'
-    position: [0, 7],
-    range: 'value'
-  },
-  {
-    element: 'close', // '|up'
-    position: [18],
-    range: 'value'
-  },
-  {
-    element: 'close', // '|up'
-    position: [1, 6],
-    range: 'value'
-  },
-  {
-    element: 'notes', // '|#'
-    position: [21],
-    range: 'sectionOperator'
-  },
-  {
-    element: 'notes', // '|#'
-    position: [2, 0],
-    range: 'sectionOperator'
-  },
-  {
-    element: 'notes', // 'note|s'
-    position: [27],
-    range: 'key'
-  },
-  {
-    element: 'notes', // 'note|s'
-    position: [2, 6],
-    range: 'key'
-  },
-  {
-    element: 'long', // '|is'
-    position: [37],
-    range: 'value'
-  },
-  {
-    element: 'long', // '|is'
-    position: [4, 0],
-    range: 'value'
-  },
-  {
-    element: 'long', // 'lo|ng'
-    position: [45],
-    range: 'key'
-  },
-  {
-    element: 'long', // 'lo|ng'
-    position: [5, 5],
-    range: 'key'
-  },
-  {
-    element: 'long', // 'long|'
-    position: [47],
-    range: 'key'
-  },
-  {
-    element: 'long', // 'long|'
-    position: [5, 7],
-    range: 'key'
+const SNIPPET_PADDING_WIDTH = 3
+const SNIPPET_PADDING = '▓'.repeat(SNIPPET_PADDING_WIDTH);
+const snippetInput = SNIPPET_PADDING + input.replace(/\n/g, '⏎').replace(/\t/g, '⇥').replace(/ /g, '␣') + ' ' + SNIPPET_PADDING;
+
+const snippet = index => {
+  index += SNIPPET_PADDING_WIDTH;
+
+  return snippetInput.substring(index - SNIPPET_PADDING_WIDTH, index) +
+         '  ' + snippetInput.charAt(index) + '  ' +
+         snippetInput.substring(index + 1, index + SNIPPET_PADDING_WIDTH + 1);
+};
+
+describe('lookup', () => {
+  let column = 0;
+  let line = 0;
+
+  let summary = '\nINDEX  SNIPPET            KEY                  RANGE\n\n';
+
+  for(let index = 0; index <= input.length; index++) {
+    const indexLookup = enolib.lookup({ index }, input);
+    const lineColumnLookup = enolib.lookup({ line, column }, input);
+
+    if(indexLookup.range !== lineColumnLookup.range)
+      throw `Lookup by index produced a different range (${indexLookup.range}) than by line/column (${lineColumnLookup.range})`;
+
+    if(indexLookup.element.stringKey() !== lineColumnLookup.element.stringKey())
+      throw `Lookup by index produced a different key (${indexLookup.element.stringKey()}) than by line/column (${lineColumnLookup.element.stringKey()})`;
+
+    const { element, range } = indexLookup;
+    const key = element.stringKey() === null ? 'document' : element.stringKey();
+    summary += `${index.toString().padEnd(5)}  ${snippet(index).padStart(9)}   =>   ${key.padEnd(20)} ${range}\n`;
+
+    if(input.charAt(index) === '\n') {
+      line++;
+      column = 0;
+    } else {
+      column++;
+    }
   }
-];
 
-describe('Element/Token lookup', () => {
-  for(let scenario of scenarios) {
-    describe(`at (${scenario.position.join(', ')})`, () => {
-
-      let lookup;
-      if(scenario.position.length === 1) {
-        lookup = enolib.lookup({ index: scenario.position[0] }, sample);
-      } else {
-        lookup = enolib.lookup({ column: scenario.position[1], line: scenario.position[0] }, sample);
-      }
-
-      it(`looks up element '${scenario.element}'`, () => {
-        expect(lookup.element.stringKey()).toEqual(scenario.element);
-      });
-
-      it(`looks up range '${scenario.range}'`, () => {
-        expect(lookup.range).toEqual(scenario.range);
-      });
-    });
-  }
+  it(`produces the expected summary`, () => {
+    expect(summary).toMatchSnapshot();
+  });
 });
