f1b53ee9b26007f168dd09922a59aa2c1d780536
Simon Repp <simon@fdpl.io>
2022-03-29 16:25:07 +0200

    Drop generative specs related to copies

--- php/spec/generated/errors/parsing/missing_element_for_continuation.spec.php
+++ php/spec/generated/errors/parsing/missing_element_for_continuation.spec.php
@@ -29,39 +29,3 @@ describe('Parsing a line continuation without any prior element', function() {
     expect($error->selection['to']['column'])->toEqual(14);
   });
 });
-
-describe('Parsing a line continuation preceded by a copied field', function() {
-  it('throws the expected ParseError', function() {
-    $error = null;
-
-    $input = "field: value\n" .
-             "\n" .
-             "copy < field\n" .
-             "| illegal_continuation";
-
-    try {
-      Enolib\Parser::parse($input);
-    } catch(Enolib\ParseError $_error) {
-      $error = $_error;
-    }
-
-    expect($error)->toBeAnInstanceOf('Enolib\ParseError');
-    
-    $text = "Line 4 contains a line continuation without a continuable element being specified before.";
-    
-    expect($error->text)->toEqual($text);
-    
-    $snippet = "   Line | Content\n" .
-               "   ...\n" .
-               "      2 | \n" .
-               "      3 | copy < field\n" .
-               " >    4 | | illegal_continuation";
-    
-    expect($error->snippet)->toEqual($snippet);
-    
-    expect($error->selection['from']['line'])->toEqual(3);
-    expect($error->selection['from']['column'])->toEqual(0);
-    expect($error->selection['to']['line'])->toEqual(3);
-    expect($error->selection['to']['column'])->toEqual(22);
-  });
-});
--- ruby/spec/generated/errors/parsing/missing_element_for_continuation.spec.rb
+++ ruby/spec/generated/errors/parsing/missing_element_for_continuation.spec.rb
@@ -23,33 +23,3 @@ describe 'Parsing a line continuation without any prior element' do
     end
   end
 end
-
-describe 'Parsing a line continuation preceded by a copied field' do
-  it 'raises the expected ParseError' do
-    input = "field: value\n" \
-            "\n" \
-            "copy < field\n" \
-            '| illegal_continuation'
-
-    begin
-      Enolib.parse(input)
-    rescue Enolib::ParseError => error
-      text = 'Line 4 contains a line continuation without a continuable element being specified before.'
-      
-      expect(error.text).to eq(text)
-      
-      snippet = "   Line | Content\n" \
-                "   ...\n" \
-                "      2 | \n" \
-                "      3 | copy < field\n" \
-                ' >    4 | | illegal_continuation'
-      
-      expect(error.snippet).to eq(snippet)
-      
-      expect(error.selection[:from][:line]).to eq(3)
-      expect(error.selection[:from][:column]).to eq(0)
-      expect(error.selection[:to][:line]).to eq(3)
-      expect(error.selection[:to][:column]).to eq(22)
-    end
-  end
-end
