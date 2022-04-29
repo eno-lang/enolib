34ec4a8176c979fde7cb1fa5d1663c8adc4200e2
Simon Repp <simon@fdpl.io>
2022-04-29 10:11:46 +0200

    Improve benchmark tooling (js)

--- javascript/performance/benchmark.js
+++ javascript/performance/benchmark.js
@@ -12,21 +12,27 @@ const SAMPLES = {
     post: fs.readFileSync(path.resolve('performance/samples/post.eno'), 'utf-8')
 };
 
-let analysis, reference;
-
 const analysisFile = path.resolve('performance/analysis.json');
 
+let analysis;
 if (fs.existsSync(analysisFile)) {
     analysis = JSON.parse(fs.readFileSync(analysisFile, 'utf-8'));
 } else {
     analysis = {};
 }
 
-reference = analysis.hasOwnProperty('reference') ? analysis.reference : null;
-analysis.modifications = { _evaluated: new Date() };
+let reference;
+if (process.argv.includes('reset') || !analysis.hasOwnProperty('reference')) {
+    reference = null;
+    delete analysis.modifications;
+    analysis.reference = { _evaluated: new Date() };
+} else {
+    reference = analysis.reference;
+    analysis.modifications = { _evaluated: new Date() };
+}
 
 for (const [name, content] of Object.entries(SAMPLES)) {
-    const before = performance.now();
+    const begin = performance.now();
     let milliseconds = 0;
     let iterations = 0;
     
@@ -36,29 +42,31 @@ for (const [name, content] of Object.entries(SAMPLES)) {
         }
         
         iterations += 1000;
-        milliseconds = performance.now() - before;
+        milliseconds = performance.now() - begin;
     }
     
     const ips = parseInt(iterations / (milliseconds / 1000.0));
-    const delta = reference ? ips - reference[name]['ips'] : 0;
     
-    let change, factor;
-    if (delta === 0) {
-        change = "~0 ips (same)";
-    } else if (delta >= 0) {
-        factor = reference ? (ips / reference[name]['ips']).toPrecision(3) : 0;
-        change = `+${delta} ips (${factor}× faster)`;
+    if (reference) {
+        const delta = ips - reference[name]['ips'];
+        
+        let change;
+        if (delta === 0) {
+            change = "~0 ips (same)";
+        } else if (delta >= 0) {
+            const factor = reference ? (ips / reference[name]['ips']).toPrecision(3) : 0;
+            change = `+${delta} ips (${factor}× faster)`;
+        } else {
+            const factor = reference ? (reference[name]['ips'] / ips).toPrecision(3) : 0;
+            change = `${delta} ips (${factor}× slower)`;
+        }
+        
+        analysis.modifications[name] = { change, ips };    
+        console.log(`${change} [${name}]`);
     } else {
-        factor = reference ? (reference[name]['ips'] / ips).toPrecision(3) : 0;
-        change = `${delta} ips (${factor}× slower)`;
+        analysis.reference[name] = { ips };
+        console.log(`${ips} ips [${name}]`);
     }
-    
-    analysis.modifications[name] = {
-        change: change,
-        ips: ips
-    };
-    
-    console.log(`${change} [${name}]`);
 }
 
 fs.writeFileSync(path.resolve('performance/analysis.json'), JSON.stringify(analysis, null, 2));
